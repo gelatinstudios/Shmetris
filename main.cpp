ea04bed4bf6c3c27e20355c357e848a9510069f1
jelly-toast <cymbaltag1017@gmail.com>
2019-11-29 20:28:58 -0500

    update

--- main.cpp
+++ main.cpp
@@ -22,62 +22,68 @@ Uint64 sdl_ticks_xxx;
 
 #endif
 
-SDL_Window *win;
-SDL_Renderer *rend;
-Tetromino current_tetromino;
-SDL_Texture *playfield_text;
-Uint32 playfield[10*20] = {};
-Uint8 timer;
+struct GameData {
+        Tetromino tetromino;
+        Uint32 playfield[10*20];
+        Uint8 timer;
+};
 
-bool handle() {
+bool handle(GameData &data) {
         bool quit = 0;
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
                 if (event.type == SDL_QUIT) {
                         quit = 1;
-                }
-
-                if (event.type == SDL_KEYDOWN) {
+                } else if (event.type == SDL_KEYDOWN) {
                         const SDL_Keycode pressed = event.key.keysym.sym;
-                        if (pressed == SDLK_z)
-                                current_tetromino.rotate_left(playfield);
-                        else if (pressed == SDLK_x)
-                                current_tetromino.rotate_right(playfield);
-                        else if (pressed == SDLK_LEFT)
-                                current_tetromino.move_left(playfield);
-                        else if (pressed == SDLK_RIGHT)
-                                current_tetromino.move_right(playfield);
+                        switch (pressed) {
+                                case SDLK_z:
+                                        data.tetromino.rotate_left(data.playfield);
+                                        break;
+                                case SDLK_x:
+                                        data.tetromino.rotate_right(data.playfield);
+                                        break;
+                                case SDLK_LEFT:
+                                        data.tetromino.move_left(data.playfield);
+                                        break;
+                                case SDLK_RIGHT:
+                                        data.tetromino.move_right(data.playfield);
+                                        break;
+                                case SDLK_DOWN:
+                                        data.timer = 0;
+                                        break;
+                        }
                 }
         }
 
-        const Uint8 *state = SDL_GetKeyboardState(0);
-        if (state[SDL_SCANCODE_DOWN]) timer = 0;
+        // const Uint8 *state = SDL_GetKeyboardState(0);
+        // if (state[SDL_SCANCODE_DOWN]) timer = 0;
 
         return quit;
 }
 
-void update() {
+void update(GameData &data) {
         //move down
-        if (!timer) {
+        if (!data.timer) {
                 for (Uint8 i = 0; i < 4; ++i) {
-                        if(current_tetromino.points[i].y < 0) continue;
-                        const bool b = playfield[coord_to_index(current_tetromino.points[i].x, current_tetromino.points[i].y + 1)];
-                        if (current_tetromino.points[i].y + 1 >= 20 || b) {
-                                current_tetromino.blit_to_playfield(playfield);
-                                current_tetromino.make_new();
+                        if(data.tetromino.points[i].y < 0) continue;
+                        const bool b = data.playfield[coord_to_index(data.tetromino.points[i].x, data.tetromino.points[i].y + 1)];
+                        if (data.tetromino.points[i].y + 1 >= 20 || b) {
+                                data.tetromino.blit_to_playfield(data.playfield);
+                                data.tetromino.make_new();
                                 break;
                         }
                 }
                 for (Uint8 i = 0; i < 4; ++i)
-                        ++current_tetromino.points[i].y;
-                timer = 10;
-        } else --timer;
+                        ++data.tetromino.points[i].y;
+                data.timer = 10;
+        } else --data.timer;
 
         //line deletion
         for (Uint8 i = 0; i < 20; ++i) {
                 bool b = 1;
                 for (Uint8 j = 0; j < 10; ++j) {
-                        if (!playfield[i * 10 + j]) {
+                        if (!data.playfield[i * 10 + j]) {
                                 b = 0;
                                 break;
                         }
@@ -85,23 +91,23 @@ void update() {
 
                 if (!b) continue;
                 for (Uint8 j = i * 10 - 1; j < i * 10; --j) {
-                        playfield[j + 10] = playfield[j];
+                        data.playfield[j + 10] = data.playfield[j];
                 }
         }
 }
 
-void render() {
+void render(SDL_Renderer *rend, SDL_Texture *playfield_text, GameData &data) {
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderClear(rend);
 
         //despite what the SDL documentation states, updating seems faster than locking
         //playfield
-        SDL_UpdateTexture(playfield_text, 0, (const void *) playfield, 4*10);
+        SDL_UpdateTexture(playfield_text, 0, (const void *) data.playfield, 4*10);
         SDL_RenderCopy(rend, playfield_text, 0, 0);
         //tetromino
         for (Uint8 i = 0; i < 4; ++i) {
-                SDL_SetRenderDrawColor(rend, rgba_colors[current_tetromino.type], rgba_colors[current_tetromino.type] >> 8, rgba_colors[current_tetromino.type] >> 16, 255);
-                const SDL_Rect rect = {current_tetromino.points[i].x, current_tetromino.points[i].y, 1, 1};
+                SDL_SetRenderDrawColor(rend, rgba_colors[data.tetromino.type], rgba_colors[data.tetromino.type] >> 8, rgba_colors[data.tetromino.type] >> 16, 255);
+                const SDL_Rect rect = {data.tetromino.points[i].x, data.tetromino.points[i].y, 1, 1};
                 SDL_RenderFillRect(rend, &rect);
                 //SDL_Log("point %u: x = %d y = %d\n", i, current_tetromino.points[i].x, current_tetromino.points[i].y);
         }
@@ -112,22 +118,24 @@ void render() {
 }
 
 int main() {
-        win = SDL_CreateWindow("Shmetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 400, 0);
-        rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
+        SDL_Window *win = SDL_CreateWindow("Shmetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 400, 0);
+        SDL_Renderer *rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_PRESENTVSYNC);
         SDL_RenderSetLogicalSize(rend, 10, 20);
 
         srand(SDL_GetPerformanceCounter());
 
-        current_tetromino.make_new();
-        playfield_text = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, 10, 20);
+        GameData data = {};
 
-        timer = 10;
+        data.tetromino.make_new();
+        SDL_Texture *playfield_text = SDL_CreateTexture(rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, 10, 20);
+
+        data.timer = 10;
 
         bool quit = 0;
         while(!quit) {
-                quit = handle();
-                update();
-                render();
+                quit = handle(data);
+                update(data);
+                render(rend, playfield_text, data);
         }
 
         SDL_DestroyRenderer(rend);
